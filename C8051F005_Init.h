742b136f264e75a02b69ea9ffa55f6285ab6134a
Murat Ownuk <ilmuratownuk@gmail.com>
2025-03-24 12:19:15 -0400

    wrote some code for device init header file

---
+++ C8051F005_Init.h
@@ -0,0 +1,19 @@
+#ifndef _C8051F005_INIT_H_ 
+    #define _C8051F005_INIT_H_ 
+
+    /*
+        defines 
+    */
+    #define SYSCLK  16000000            // chip operating frequency 
+
+    #ifndef _C8051F005_INIT_C_ 
+
+        // function prototypes (for external use) 
+        // prototypes 
+
+    #endif
+
+#endif
+
+
+
